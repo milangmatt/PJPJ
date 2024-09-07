005a897b6debe8fb2b8268d18d6b76aab8891f9b
milangmatt <milangeorgem@gmail.com>
2024-09-07 21:58:35 +0530

    game logic done

--- sketch.js
+++ sketch.js
@@ -1,28 +1,44 @@
 
 let cur=0;
+let poocur=0;
+let ranpoo;
 let ran;
 let score=0;
+let curspeed=3;
+
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(500, 500);
   ran=random(45,width-45);
+  ranpoo=random(45,width-45);
 
+  highScore = getItem('HS');
+  if (highScore === null) {
+    highScore = 0;
+  }
+  
 }
 
 function draw() {
   // put drawing code here
   background(0);
-  displayStartMessage();
+  startGame();
+}
+
+function startGame(){
+  displayScore();
+  fill(77, 61, 18)
+  ellipse(ranpoo,poocur,32,50);
   fill(255)
-  
-  ellipse(ran,cur,32,32);
+  ellipse(ran,cur,32,40);
   rect(mouseX,height-45,60,32);
-  cur+=3
-  
+  cur+=curspeed
+  poocur+=6
 
   if (((cur>=height-45))&(((mouseX+60)>=ran)&(mouseX<=ran))) {
     score +=1;
     cur=0;
+    curspeed+=0.25;
     ran=random(45,width-45);
   }
   if (cur>=height){
@@ -30,17 +46,52 @@ function draw() {
     score=score-1;
     ran=random(45,width-45);
   }
+  if(poocur>=height){
+    poocur=0;
+    ranpoo=random(45,width-45);
+  }
+  if (((poocur>=height-45))&(((mouseX+60)>=ranpoo)&(mouseX<=ranpoo))) {
+    gameOver();
+   
+    score=0;
+    poocur=0;
+    ranpoo=random(45,width-45);
+    
+  }
 
-  
-
+function gameOver(){
+ 
+  noLoop();
+  highScore = max(highScore, score);
+  storeItem('HS', highScore);
+  curspeed=3
+  background(0)
+  textSize(48);
+  textAlign(CENTER);
+  textFont('Arial');
+  fill(255, 0, 0);
+  text(`Game Over`, width/2, height/3);
+  textSize(20);
+  textAlign(CENTER);
+  textFont('Courier New');
+  fill(255, 0, 0);
+  text(`HighScore: ${highScore}\nScore : ${score}\n\n Tap to Play Again`, width/2, height/2);
 }
 
-function displayStartMessage() {
+}
+function displayScore() {
   textSize(20);
   textAlign(CENTER);
 
   //Add font
   textFont('Courier New');
   fill(255, 0, 0);
-  text(`Score : ${score}`, width*(7/8), height/8);
+  text(`Score : ${score}`, width/8, height/8);
+}
+
+function mousePressed(){
+  if(isLooping()===false){
+    startGame();
+    loop();
+  }
 }
